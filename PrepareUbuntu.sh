51f1a28b320b7ab26a859693c899e63b5d76dd2f
Edwin Mantilla <hackpro.ems@gmail.com>
2018-11-14 09:31:34 -0500

    Add steps for install Postgres

--- PrepareUbuntu.sh
+++ PrepareUbuntu.sh
@@ -60,6 +60,17 @@ code --install-extension eg2.vscode-npm-script
 code --install-extension donjayamanne.githistory
 
 
+######################
+# Install PostgreSQL #
+######################
+# -Source: https://wiki.postgresql.org/wiki/Apt
+sudo apt-get install postgresql postgresql-contrib
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get update
+sudo apt-get install pgadmin4
+
+
 ###############################
 # Prepare GitHub Repositories #
 ###############################
