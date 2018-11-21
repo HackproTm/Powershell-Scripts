3acf2b64e877fb8f5bab0c6b0e8dda76652f6d1d
Edwin Mantilla <hackpro.ems@gmail.com>
2018-11-21 14:59:50 -0500

    Use tmp folder for install PostgreSQL

--- PrepareUbuntu.sh
+++ PrepareUbuntu.sh
@@ -100,7 +100,8 @@ code --install-extension donjayamanne.githistory
 ######################
 # -Source: https://wiki.postgresql.org/wiki/Apt
 sudo apt-get install postgresql postgresql-contrib
-curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+curl -o /tmp/ACCC4CF8.asc https://www.postgresql.org/media/keys/ACCC4CF8.asc
+sudo apt-key add /tmp/ACCC4CF8.asc
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 sudo apt-get update
 sudo apt-get install pgadmin4
