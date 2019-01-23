18537fac60e4a8b624e2d327a68ac0139f5c4898
Edwin Mantilla <hackpro.ems@gmail.com>
2019-01-23 10:47:18 -0500

    Update script for manage multiple network interfaces.

--- Disconnect.bat
+++ Disconnect.bat
@@ -3,4 +3,4 @@ ECHO @Off
 SET ScriptPath=%~dp0
 CD /D %ScriptPath%
 
-powershell.exe -ExecutionPolicy ByPass -NonInteractive -Command "& {.\SetIPRoute.ps1}"
+powershell.exe -ExecutionPolicy ByPass -Command "& {.\SetIPRoute.ps1}"
--- SetIPRoute.ps1
+++ SetIPRoute.ps1
@@ -2,17 +2,50 @@ Param(
 	[parameter(Mandatory=$False, Position=0)][switch]$Create
 )
 
-$ConfirmPreference = 'None'; # See 'Get-Help about_Preference_Variables';
-$IpIgnore = "10", "127", "224", "255";
-$InterfaceIndex = 10;        # Get via 'ifIndex' using 'Get-NetAdapter -Name "*" | Format-Table -Property "Name", "InterfaceDescription", "ifIndex", "Status"'
-$Gateway = "172.28.200.1";   # Get via 'Get-NetRoute -DestinationPrefix "0.0.0.0/0" | Select  NextHop'
+$ConfirmPreference = 'None';	# See 'Get-Help about_Preference_Variables';
+$IpIgnore = "127", "224", "255";
 $PslIp = @("10.10.10.0/24", "10.10.1.0/24", "10.20.1.0/24", "192.168.151.13/32");
+
+$Adapters = Get-NetAdapter -Name "*" | Where-Object -Property Status -Eq "Up";
+$Adapters | Format-Table;
+
+If ($Adapters.Count -Gt 1)
+{
+    $Options = New-Object System.Collections.ObjectModel.Collection[System.Management.Automation.Host.ChoiceDescription];
+    $I = 0;
+    
+	ForEach($Adapter In $Adapters)
+	{
+        $I++;
+        $Choice = "&" + $I + " - " + $Adapter.Name;
+        $Options.Add((New-Object System.Management.Automation.Host.ChoiceDescription $Choice, $Adapter.InterfaceDescription)) > $null;
+	}
+    $Options.Add((New-Object System.Management.Automation.Host.ChoiceDescription "&Q - Quit", "Quit")) > $null;
+
+    $Result = $Host.UI.PromptForChoice("Multiple network interfaces were detected.", "Choice the ifIndex from interface to configure?", $Options, 0);
+    If ($Result -Lt 0 -Or $Result -Eq $Options.Count - 1)
+    {
+        Exit;
+    }
+
+    $InterfaceIndex = (Get-NetRoute -DestinationPrefix "0.0.0.0/0" | Where-Object ifIndex -Eq $Adapters[$Result].ifIndex).ifIndex;
+	$Gateway = (Get-NetRoute -DestinationPrefix "0.0.0.0/0" | Where-Object ifIndex -Eq $Adapters[$Result].ifIndex).NextHop;
+}
+Else
+{
+	$InterfaceIndex = (Get-NetRoute -DestinationPrefix "0.0.0.0/0").ifIndex;
+	$Gateway = (Get-NetRoute -DestinationPrefix "0.0.0.0/0").NextHop;
+}
 
 ForEach ($Ip In $PslIp)
 {
 	$Exist = Get-NetRoute -DestinationPrefix $Ip -AddressFamily IPv4 -PolicyStore ActiveStore -ErrorAction SilentlyContinue;
 	If ($Exist)
 	{
+		If (!$Create)
+		{
+			Write-Host "Removing... " $Ip
+		}
 		Remove-NetRoute -DestinationPrefix $Ip -AddressFamily IPv4 -PolicyStore ActiveStore -Confirm:$False -ErrorAction SilentlyContinue;
 	}
 	If ($Create)
@@ -21,14 +54,18 @@ ForEach ($Ip In $PslIp)
 	}
 }
 
-For ($Ip = 1; $Ip -lt 255; $Ip++)
+For ($I = 1; $I -lt 255; $I++)
 {
-	If ($IpIgnore.IndexOf($Ip.ToString()) -Eq -1)
+	If ($IpIgnore.IndexOf($I.ToString()) -Eq -1)
 	{
-		$Route = $Ip.ToString() + ".0.0.0/8";
+		$Route = $I.ToString() + ".0.0.0/8";
 		$Exist = Get-NetRoute -DestinationPrefix $Route -AddressFamily IPv4 -PolicyStore ActiveStore -ErrorAction SilentlyContinue;
 		If ($Exist)
 		{
+			If (!$Create)
+			{
+				Write-Host "Removing... " $Route
+			}
 			Remove-NetRoute -DestinationPrefix $Route -AddressFamily IPv4 -PolicyStore ActiveStore -Confirm:$False -ErrorAction SilentlyContinue;
 		}
 		If ($Create)
